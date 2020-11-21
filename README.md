917bb025324bd6305c70a556f11535d40c0d95a7
Britnell <tommy.britnell@gmail.com>
2020-11-21 13:41:18 +0100

    first dump of all these files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # VectorNode
 My repo of vector based generative art with paper.js with a node server for local development, 
+
+All of these (and more) are served on my website over at [Webs.Wonday.eu](https://webs.wonday.eu/). Some of them are just work in progress or quick tests of specific functions or methods.
+
+# use 
+
+npm install the modules
+
+then run the server with 
+`node socket_server.js`
+
+this will run localhost:8000
+the pages are served statically so just open them with 
+
+`localhost:8000/html/FILENAME.html`
+
---
+++ socket_server.js
@@ -0,0 +1,34 @@
+/*
+    # # # # ------------   
+*/
+
+
+
+	// * Setup
+const express = require('express'); // running immediately
+
+const app = require('express')(); // running immediately
+const server = require('http').Server(app);
+
+const port = 3000;
+
+// server.listen(port, () =>  {
+//   console.log(`Server is runnign port ${port} `);
+// } );
+
+	// * Express
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname +'/html/paper_vector.html');
+});
+
+// app.get('/list-letters', (req,res)=>{
+// });
+app.use('/html', express.static('html'));
+app.use('/js', express.static('js'));
+app.use('/static', express.static('static'));
+app.use('/letters', express.static('letters'));
+app.use('/src', express.static('src'));
+
+console.log(`App is runnign port :8000 `);
+app.listen(8000);
